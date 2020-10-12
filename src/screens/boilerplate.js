044ef269b72d986c1d8d06df5622ecd7fb99524c
abdulhamidnugroho <abdulhamidnugroho@gmail.com>
2020-10-12 10:19:04 +0700

    generating random color (state management)

---
+++ src/screens/boilerplate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'react-native';
+import { StyleSheet, View, FlatList } from 'react-native';
+
+const ColorScreen = () => {
+    return <View>
+        <Button title="Add a Color" />
+    </View>
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default CounterScreen;
